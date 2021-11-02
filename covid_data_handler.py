0cb8f1af7b384c9fed08f42e32c4e4e94095be79
Ethan Ray <ethan.gregory.ray@gmail.com>
2021-11-02 15:35:55 +0000

    test_process_covid_csv_data test case added

--- covid_data_handler.py
+++ covid_data_handler.py
@@ -1,5 +1,5 @@
 def parse_csv_data(csv_filename):
-    f = open(csv_filename).read().split("\n")
+    f = open(csv_filename).read().split("\n") ## open and process file by line
     f.pop(-1) ## Remove Blank line
     return f
 
