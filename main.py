d42cb40b8a8c3abe9b03abe3e3629736e8d7f64e
Ethan Ray <ethan.gregory.ray@gmail.com>
2021-11-05 12:53:37 +0000

    flask stuff

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 from flask import Flask, render_template ,redirect,request
+
+from covid_data_handler import *
+from covid_news_handling import *
+
+nation="england"
+
+
+
+
+
 app = Flask(__name__)
+
+
 
 @app.route("/index")
 def index():
+    update_covid_data()
+    update_news()
+    newCases = 0
+    for x in range(0,7):
+        newCases += covid_data[x]["newCasesByPublishDate"]
+        
+    nation_data = covid_API_request("england","nation")
+    nationCases = 0
+    for x in range(0,7):
+        nationCases += nation_data[x]["newCasesByPublishDate"]
+    
+    print(request.args)
+    #last7DaysInfections = int(covid_data[2]["cumCasesByPublishDate"]) - int(covid_data[8]["cumCasesByPublishDate"])
     ## Var decomp
     ## updates
     ## title
@@ -13,7 +38,8 @@ def index():
     ## hospital_cases
     ## deaths_total
     ## news_articles
-    return render_template("index.html",updates="",title="Hello World!")
+    ##notification
+    return render_template("index.html",local_7day_infections=newCases,updates="",nation_location=nation_data[0]["areaName"],national_7day_infections=nationCases,title="Hello World!",location=covid_data[0]["areaName"],news_articles=news_articles,notification={'title':'Test'})
 
 
 app.run(port=5002,debug=True)
