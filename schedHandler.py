20f056baf98c1e9149419b7a283f3ef56caaad90
Ethan Ray <ethan.gregory.ray@gmail.com>
2021-11-17 20:26:36 +0000

    error fixing & cleanup

--- schedHandler.py
+++ schedHandler.py
@@ -8,16 +8,23 @@ class schedHandler:
     def addEvent(self,event,update_interval,update_name,info,*args,**kwargs):
         repeater = None
         info = str(info)
-        main=self.schObj.enter(update_interval,1,event,argument=(args),kwargs=kwargs)
+        main=self.schObj.enter(update_interval,1,event,argument=(args))
         events = [main]
         if "repeat" in kwargs :
             if kwargs["repeat"] == True:
                 repeater=self.schObj.enter(update_interval,2,self.addEvent,argument=(event,24*60*60,update_name,info,args,),kwargs=kwargs)
                 events.append(repeater)
-
+        
         info = {"content":info,"title":update_name,"events":events}
+
+        self.schObj.enter(update_interval,3,self.__cleanup,argument=(info,))
+
         self.events.append(info)
         self.runSched()
+    def __cleanup(self,info):
+        for item in self.events:
+            if info == item:
+                self.events.remove(item)
     def getEvents(self):
         return self.events
     def removeEvent(self,e):
